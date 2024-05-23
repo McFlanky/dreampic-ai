6a7423e113e5b464a6a37e1fd9daacec93206562
McFlanky <cturjanica02@gmail.com>
2024-05-23 17:53:42 -0400

    Basic server and makefile with templ downlaoded

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/McFlanky/dreampic-ai
 
 go 1.22.1
 
-require github.com/go-chi/chi/v5 v5.0.12 // indirect
+require (
+	github.com/a-h/templ v0.2.697 // indirect
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := initEverything(); err != nil {
+		log.Fatal(err)
+	}
+	router := chi.NewMux()
+
+	port := os.Getenv("HTTP_LISTEN_ADDR")
+	slog.Info("Starting server on ", "port", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func initEverything() error {
+	return godotenv.Load()
 }
